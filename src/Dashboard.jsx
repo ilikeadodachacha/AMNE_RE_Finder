87fb3770db6cbbb25d41b2760dbb6052d6fecd5a
Landon Roberts <landonmarkroberts@gmail.com>
2018-01-31 11:24:37 -0600

    implement handleChange for form inputs

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -4,16 +4,23 @@ class Dashboard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      addr1: '',
+      addr2: '',
+      agencies: ''
     }
+  }
+
+  handleChange(e) {
+    e.preventDefault();
+    console.log('handle change hit!')
   }
 
   render() {
     return (
       <div> 
         <form className="Search-form">
-          Address1: <input type="text" name="addr1" value="test"/><br />
-          Address2: <input type="text" name="addr2" value="test"/><br />
+          Address1: <input type="text" name="addr1" value="test" onChange={this.handleChange}/><br />
+          Address2: <input type="text" name="addr2" value="test" onChange={this.handleChange}/><br />
           <input type="submit" />
         </form>
       </div>
