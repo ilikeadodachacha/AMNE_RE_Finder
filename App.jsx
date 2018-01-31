ad43d0ee1e7685ae32e6eb5a571718fca246297f
Landon Roberts <landonmarkroberts@gmail.com>
2018-01-31 10:43:21 -0600

    initialize repo, install dependecies and build basic components

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div> 
+        Up and running!
+      </div> 
+    )
+  }
+}
+
+export default App;
