d3c8cedc8c08ca57b162c3331b7ea9328431791a
Landon Roberts <landonmarkroberts@gmail.com>
2018-01-31 10:48:07 -0600

    Create new component - Dashboard

--- App.jsx
+++ App.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Dashboard from './Dashboard.jsx';
 
 class App extends React.Component {
   render() {
     return (
       <div> 
-        Up and running!
+        <Dashboard />
       </div> 
     )
   }
